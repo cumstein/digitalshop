2731456a39fd209c7a7ab59fc65fe4a24ffba12c
Cumstein <kamyabaghaz@gmail.com>
2025-03-30 09:10:58 +0330

    adding tailwind and shadcn, starting on homepage with banner and welcome components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
+import Banner from "@/components/banner";
+import Welcome from "@/components/Welcome";
+
 export default function Home() {
-  // console.log("test");
-  const renderName = (name2: any) => {
-    const name: string = 'Kamy';
-    return <span>{name}</span>;
-  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div>my fist page</div>
-      {renderName(2)}
+    <div className="flex flex-col items-center justify-center h-screen" >
+      <Banner />
+      <Welcome />
     </div>
   );
 }
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,19 @@
+import { MonitorSmartphone } from "lucide-react"
+import { Button } from "./ui"
+import Link from "next/link"
+
+function Welcome() {
+  return (
+    <div className="flex flex-col justify-center items-center mt-6">
+        <span>Welcome to</span>
+        <div className="flex items-center gap-3">
+            <MonitorSmartphone />
+            <p className="text-2xl font-bold text-gray-800 ">Digital Shop</p>
+        </div>
+        <Button asChild className="mt-6">
+            <Link href="/products">Go to Products</Link>
+        </Button>
+    </div>
+  )
+}
+export default Welcome
